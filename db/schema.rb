2f0bdf24fcf997cea66b683ffcc073a93a313384
Pedro Cezar <pedrofcezar@gmail.com>
2018-07-30 15:13:47 -0300

    if implemente this commit will create a DB called pets

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_30_175405) do
+ActiveRecord::Schema.define(version: 2018_07_30_181012) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "pets", force: :cascade do |t|
+    t.string "name"
+    t.string "species"
+    t.string "address"
+    t.string "photo"
+    t.text "details"
+    t.string "size"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_pets_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -32,4 +45,5 @@ ActiveRecord::Schema.define(version: 2018_07_30_175405) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "pets", "users"
 end

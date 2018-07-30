fcbdcdd03debc7276cb05eee77dfa5d9a25edf10
Brady Clark <bradyjclark88@gmail.com>
2018-07-30 20:58:34 -0300

    add pets index to auth whitelist

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,6 +1,7 @@
 class PetsController < ApplicationController
   before_action  :set_user
   before_action :set_pet, only: [:show, :update, :edit, :destroy]
+  skip_before_action :authenticate_user!, only: [:index]
 
   def index
     @pets = Pet.all

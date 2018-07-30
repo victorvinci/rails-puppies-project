a82756d713b59fe639b2ff58cfa3a1ccbdd030a8
Pedro Cezar <pedrofcezar@gmail.com>
2018-07-30 15:48:08 -0300

    if implemented this will generate our pets routes

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,10 +1,10 @@
 class Pet < ApplicationRecord
-  belongs_to :user, class_name: "User", foreign_key: "owners_id"
+  belongs_to :user, class_name: "User", foreign_key: "owner_id"
   has_many :bookings
   validates :name, presence: true
   validates :species, presence: true
   validates :address, presence: true
   validates :size, inclusion: { in: ("microscopic", "small", "medium", "large", "extra-large", "monster-sized"),
     message: "Not a valid size" }
-  validates :detail, length: { minimum: 30 }
+  validates :details, length: { minimum: 30 }
 end

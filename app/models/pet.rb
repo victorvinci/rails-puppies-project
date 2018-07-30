e4247e78d38f76ec12964bf7a7943059617e1682
Pedro Cezar <pedrofcezar@gmail.com>
2018-07-30 15:33:35 -0300

    if implemented this commit will make the associations and validations to pets model

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,3 +1,10 @@
 class Pet < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, class_name: "User", foreign_key: "owners_id"
+  has_many :bookings
+  validates :name, presence: true
+  validates :species, presence: true
+  validates :address, presence: true
+  validates :size, inclusion: { in: ("microscopic", "small", "medium", "large", "extra-large", "monster-sized"),
+    message: "Not a valid size" }
+  validates :detail, length: { minimum: 30 }
 end

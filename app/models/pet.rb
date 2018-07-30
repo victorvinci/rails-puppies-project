a0e94c74e57a9e0137e2338f792e2627a8fba2c5
Brady Clark <bradyjclark88@gmail.com>
2018-07-30 16:40:26 -0300

    Add db seed

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,10 +1,10 @@
 class Pet < ApplicationRecord
-  belongs_to :user, class_name: "User", foreign_key: "owner_id"
+  belongs_to :owner, class_name: "User", foreign_key: "user_id"
   has_many :bookings
   validates :name, presence: true
   validates :species, presence: true
   validates :address, presence: true
   validates :size, inclusion: { in: ["microscopic", "small", "medium", "large", "extra-large", "monster-sized"],
     message: "Not a valid size"}
-  validates :details, length: { minimum: 30 }
+#  validates :details, length: { minimum: 10 }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,23 @@
+# Destroy all previous db entries
 Pet.destroy_all
-20.times do
-  pet = Pet.new(
+User.destroy_all
+
+5.times do
+  user = User.new(
+  email: Faker::Internet.email,
+  password: Faker::Seinfeld.character,
+)
+user.save!
+pet = Pet.new(
     name: Faker::Dog.name,
     species: "dog",
     address: Faker::Address.full_address,
     size: ["microscopic", "small", "medium", "large", "extra-large", "monster-sized"].sample,
     details: Faker::Seinfeld.quote,
+    owner: user
     )
-  pet.save
+  pet.save!
 end
+
+
+

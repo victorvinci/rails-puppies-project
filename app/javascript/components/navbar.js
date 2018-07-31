7b5823cd485ba4755b9e86e06ae86ca5ce350355
Brady Clark <bradyjclark88@gmail.com>
2018-07-30 22:00:27 -0300

    Fixed my ballsups

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -12,3 +12,17 @@ function initUpdateNavbarOnScroll() {
 }
 
 export { initUpdateNavbarOnScroll };
+
+function initUpdateNavbarOnScroll() {
+  const navbar = document.querySelector('.navbar-wagon');
+  if (navbar) {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY >= window.innerHeight) {
+        navbar.classList.add('navbar-wagon-white');
+      } else {
+        navbar.classList.remove('navbar-wagon-white');
+      }
+    });
+  }
+}
+ export default initUpdateNavbarOnScroll;

341d86ec97261c8cf826900138e063382714666c
Brady Clark <bradyjclark88@gmail.com>
2018-07-31 14:46:54 -0300

    fix pundit show logic

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :pets do
-    resources :booking, except: %i[index destroy] do
+    resources :bookings, except: %i[index destroy] do
       resources :review, except: %i[index destroy]
     end
   end

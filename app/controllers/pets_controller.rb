1939af96782bc47e00fa010ac52f2ee688afc532
Pedro Cezar <pedrofcezar@gmail.com>
2018-07-31 14:29:32 -0300

    if implemented, this will create a container in the index page

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -5,7 +5,7 @@ class PetsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
-    @pets = policy_scope(Pet) #Pet.all
+    @pets = policy_scope(Pet)
   end
 
   def show

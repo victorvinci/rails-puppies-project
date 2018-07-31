225a1a7482f28f9887b32ee90bdd5322d2b9a244
Brady Clark <bradyjclark88@gmail.com>
2018-07-31 18:21:50 -0300

    Add basic logic, pundit and path

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,5 @@
 class Review < ApplicationRecord
-  belongs_to :bookings
+  belongs_to :booking
+  validates :pet_rating, numericality: { only_integer: true,
+    greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
 end

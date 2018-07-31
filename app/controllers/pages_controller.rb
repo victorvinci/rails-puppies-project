c4858f163a4b1f3034eb8aace0812533548a798c
Pedro Cezar <pedrofcezar@gmail.com>
2018-07-31 17:07:18 -0300

    if implemented this will create a results action in page, a results route and a results view for the search in home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,12 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home, :about]
+  skip_before_action :authenticate_user!, only: [:home, :results, :about]
   def home
   end
+
+  def results
+    @results = Pet.main_search(params[:query])
+  end
+
+
+
 end

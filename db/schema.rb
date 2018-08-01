3500e6f9b7f26da47015f792ddc3a7c9b3946fb4
Brady Clark <bradyjclark88@gmail.com>
2018-08-01 10:09:49 -0300

    Geocoding implemented for pet

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_01_124341) do
+ActiveRecord::Schema.define(version: 2018_08_01_130410) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,7 +37,7 @@ ActiveRecord::Schema.define(version: 2018_08_01_124341) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "photo"
-    t.float "latitidue"
+    t.float "latitude"
     t.float "longitude"
     t.index ["user_id"], name: "index_pets_on_user_id"
   end

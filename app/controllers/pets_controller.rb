e809deff55fb25e25b9699baec4169f2cadf858a
Brady Clark <bradyjclark88@gmail.com>
2018-08-01 15:27:50 -0300

    edit path changed

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -39,7 +39,7 @@ class PetsController < ApplicationController
 
   def update
     if @pet.update(pet_params)
-      redirect_to pet_path(@pet), notice: 'Pet was successfully updated.'
+      redirect_to pets_path, notice: 'Pet was successfully updated.'
     else
       render :edit
     end
